d9ca6548d43a82a4a8ec4bd9c6b954bb66cc3c34
AshleySetter <ajs3g11@soton.ac.uk>
2017-07-01 00:54:07 +0100

    updated setup.py and added __init__ file so that package can be installed correctly

--- frange/setup.py
+++ frange/setup.py
@@ -5,19 +5,19 @@ mypackage_root_dir = os.path.dirname(__file__)
 with open(os.path.join(mypackage_root_dir, 'requirements.txt')) as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-with open(os.path.join(mypackage_root_dir, 'qplots/VERSION')) as version_file:
+with open(os.path.join(mypackage_root_dir, 'frange/VERSION')) as version_file:
     version = version_file.read().strip()
 
 
-setup(name='qplots',
+setup(name='frange',
       version=version,
-      description='Python package with functions for plotting',
+      description='Python package with frange data type for storing large arrays of equally separated data',
       author='Ashley Setter',
       author_email='A.Setter@soton.ac.uk',
-      url="https://github.com/AshleySetter/qplots",
-      download_url="https://github.com/AshleySetter/qplots/archive/{}.tar.gz".format(version),
+      url="https://github.com/AshleySetter/frange",
+      download_url="https://github.com/AshleySetter/frange/archive/{}.tar.gz".format(version),
       include_package_data=True,
-      packages=['qplots',
+      packages=['frange',
       ],
       install_requires=requirements,
 )
