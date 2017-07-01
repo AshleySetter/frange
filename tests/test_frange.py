5e251956ea86709f64d9bdca6d99f2b167af2617
AshleySetter <ajs3g11@soton.ac.uk>
2017-07-01 02:34:43 +0100

    imrpoved test coverage

--- tests/test_frange.py
+++ tests/test_frange.py
@@ -7,14 +7,14 @@ These tests test that frange behaviour matches np.arange's
 behaviour, as is desired, such that they can be used iterchangably.
 """
 
-def test_frange_postiveStepSize():
+def test_frange_get_array_postiveStepSize():
     """
     Tests that for positive step size frange's returned array matches arange's
     """
     np.testing.assert_allclose(frange(0, 10, 0.1).get_array(), np.arange(0, 10, 0.1), rtol=1e-10)
     return None
 
-def test_france_nosteps():
+def test_frange_get_array_nosteps():
     """
     Tests that for positive step size, with zero steps in interval, 
     frange's returned array matches arange's, which should be empty.
@@ -22,7 +22,7 @@ def test_france_nosteps():
     np.testing.assert_allclose(frange(10, 0, 0.1).get_array(), np.arange(10, 0, 0.1), rtol=1e-10)
     return None
     
-def test_france_negtaiveStepSize():
+def test_frange_get_array_negtaiveStepSize():
     """
     Tests that for negative step size frange's returned array matches arange's
     """
@@ -38,6 +38,28 @@ def test_raise_zeroDivisionError():
         t = frange(1, 0, 0)
     return None
 
+def test_frange_get_generator_postiveStepSize():
+    """
+    Tests that for positive step size frange's returned generator matches arange's
+    """
+    np.testing.assert_allclose(list(frange(0, 10, 0.1).get_generator()), np.arange(0, 10, 0.1), rtol=1e-10)
+    return None
+
+def test_frange_get_generator_nosteps():
+    """
+    Tests that for positive step size, with zero steps in interval, 
+    frange's returned generator matches arange's, which should be empty.
+    """
+    np.testing.assert_allclose(list(frange(10, 0, 0.1).get_generator()), np.arange(10, 0, 0.1), rtol=1e-10)
+    return None
+    
+def test_frange_get_generator_negtaiveStepSize():
+    """
+    Tests that for negative step size frange's returned generator matches arange's
+    """
+    np.testing.assert_allclose(list(frange(10, 0, -0.1).get_generator()), np.arange(10, 0, -0.1), rtol=1e-10)
+    return None
+    
 def test_length():
     assert len(frange(0, 10, 0.1).get_array()) == 100
     return None
